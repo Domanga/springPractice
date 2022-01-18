e289ae387026724b6835b904af97ff726a42fdf6
Domanga <dhkdndi7@naver.com>
2022-01-18 18:43:20 +0900

    index.html, js, css추가

---
+++ prac02/src/main/java/com/inho/prac02/controller/SearchRequestController.java
@@ -0,0 +1,23 @@
+package com.inho.prac02.controller;
+
+import com.inho.prac02.models.ItemDto;
+import com.inho.prac02.utils.NaverShopSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final이 포함된 클래스를 생성하도록
+@RestController
+public class SearchRequestController {
+
+    private final NaverShopSearch naverShopSearch;
+
+    @GetMapping("/api/search")
+    public List<ItemDto> getItems(@RequestParam String query) {
+        String resultString = naverShopSearch.search(query);
+        return naverShopSearch.fromJSONtoItems(resultString);
+    }
+}
